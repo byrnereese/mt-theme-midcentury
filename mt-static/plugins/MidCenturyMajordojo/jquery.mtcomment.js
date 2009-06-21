39190ee0193e396ef6bc733188e785b415302fde
Byrne Reese <byrne@majordojo.com>
2009-06-21 09:12:00 -0700

    More improvements to ajax commenting. Upgraded jquery plugin with latest version. Fixed comment response template in regards to how it returns errors.

--- mt-static/plugins/MidCenturyMajordojo/jquery.mtcomment.js
+++ mt-static/plugins/MidCenturyMajordojo/jquery.mtcomment.js
@@ -1,69 +1,77 @@
 (function($){
   $.fn.reply = function(options) {
     var defaults = {
-    speed: 'slow',
-    leaveCommentMsg: 'Leave a comment...'
+      speed: 'slow',
+      entryId: null,
+      formSource: '#comments-form',
+      formClass: 'ajax-comments-form',
+      leaveCommentMsg: 'Leave a comment...'
     };
     var self;
     var settings = $.extend( {}, defaults, options);
     var clicked = Array();
+    var onReplyClick = function() {
+      var replyLink = $(this);
+      var pid_e = $(this).closest('.comment');
+      var pid = pid_e.attr('id').substr(8);
+      var pauthor_e = $(this).closest('.byline').find('.vcard.author');
+      var pauthor = pauthor_e.html();
+      var form = clicked[pid];
+      if (!form) {
+        clicked[pid] = initForm( replyLink, pid, pid_e );
+        $(this).addClass('expanded');
+        clicked[pid].slideDown(settings.speed);
+      } else if (form.hasClass('expanded')) {
+        $(this).siblings('.'+settings.formClass).slideUp(settings.speed);
+        $(this).removeClass('expanded');
+        $(this).addClass('collapsed');
+      } else if (form.hasClass('collapsed')) {
+        $(this).siblings('.'+settings.formClass).slideDown(settings.speed);
+        $(this).addClass('expanded');
+        $(this).removeClass('collapsed');
+      } else {
+        alert("Fatal error. A form was never initialized for this element.");
+      }
+    };
     return this.each(function() {
         obj = $(this);
         self = obj;
-        self.click( function() { 
-            var replyLink = $(this);
-            var pid_e = $(this).closest('.comment');
-            var pid = pid_e.attr('id').substr(8);
-            var pauthor_e = $(this).closest('.byline').find('.vcard.author');
-            var pauthor = pauthor_e.html();
-            if (clicked[pid] == 1) {
-              clicked[pid] = 2;
-              $(this).siblings('#comments-form').slideUp(settings.speed);
-              $(this).removeClass('expanded');
-            } else if (clicked[pid] == 2) {
-              clicked[pid] = 1;
-              $(this).siblings('#comments-form').slideDown(settings.speed);
-              $(this).addClass('expanded');
-            } else {
-              clicked[pid] = 1;
-              var form_html = $('#comments-form').html();
-              var form = $(this).after('<form id="comments-form">'+form_html+'</form>').siblings('#comments-form');
-              form.attr({
-                method: 'post',
-                action: mt.blog.comments.script
-              });
-              // Create containers for status messaging
-              $(this).addClass('expanded');
-              form.append('<div id="spinner"></div><div id="spinner-status"></div>');
-              //form.find('input#comment-submit').replaceWith('<input type="button" value="Submit" id="comment-submit" />');
-              form.find('textarea').unbind('focus').val(settings.leaveCommentMsg).focus( function() { $(this).val(''); });
-              form.find('input#comment-preview').replaceWith('');
-              form.slideDown(settings.speed);
-              form.unbind('submit');
-              form.submit( function(e){
-                  // flag this as an ajax request
-                  // TODO - use classes, not IDs
-                  // TODO - prime new comments with proper callbacks (reply())
-                  // TODO - add config variables
-                  form.find('#ajax').val('1');
-                  form.find('#parent_id').val(pid);
-                  form.find('#armor').val(mt.blog.comments.armor);
-                  $(this).ajaxSubmit({
-                    contentType: 'application/x-www-form-urlencoded; charset=utf-8',
-                    beforeSubmit: function(formData, jqForm, options) {
-                      var queryString = $.param(formData); 
-                      alert('About to submit: \n\n' + queryString); 
-                      $('#spinner, #spinner-status').fadeIn('fast').css('height',form.height());
-                    },
-                    success: function(responseText, statusText) {
-                      $('#spinner, #spinner-status').fadeOut( function() { form.hide(); replyLink.removeClass('expanded'); });
-                      pid_e.after(responseText).siblings('.comment').fadeIn('slow');
-                    }
-                  });
-                  return false;
-              });
-            }
+        self.click( onReplyClick );
+      });
+    function initForm( target, parent, parent_elem ) {
+      var form_html = $(settings.formSource).html();
+      var f = target.after('<form class="'+settings.formClass+'">'+form_html+'</form>').siblings('.'+settings.formClass);
+      f.css({ 'display':'none', 'position':'relative','margin':'0','padding':'0' });
+      f.append('<div id="spinner"></div><div id="spinner-status"></div>');
+      f.find('textarea').unbind('focus').val(settings.leaveCommentMsg).focus( function() { $(this).val(''); });
+      f.find('input#comment-preview').replaceWith('');
+      f.unbind('submit');
+      f.submit( function(e){
+          // TODO - use classes, not IDs
+          // TODO - add config variables
+          $(this).find('#ajax').val('1');
+          $(this).find('#parent_id').val(parent);
+          $(this).find('#armor').val(mt.blog.comments.armor);
+          // TODO - fix! this is not submitting any more
+          $(this).ajaxSubmit({
+              contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+              url: mt.blog.comments.script,
+              type: 'post',
+              clearForm: true,
+              beforeSubmit: function(formData, jqForm, options) {
+                var queryString = $.param(formData); 
+                $('#spinner, #spinner-status').fadeIn('fast').css('height',f.height());
+              },
+              success: function(responseText, statusText) {
+                $('#spinner, #spinner-status').fadeOut( function() { f.hide(); target.removeClass('expanded'); });
+                var comment = parent_elem.after(responseText).siblings('.comment');
+                comment.fadeIn('slow');
+                comment.find('a.reply').click( onReplyClick );
+              }
+            });
+          return false;
         });
-      });       
+      return f;
+    };
   };
 })(jQuery);
