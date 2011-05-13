c64c68d8e68e8ed5c5dd51a3a5edabebfc46a26d
Byrne Reese <byrne@majordojo.com>
2011-05-13 09:13:25 -0700

    Added L10N classes. They are empty, but it might resolve a problem with MT5

---
+++ plugins/MidCentury/lib/MidCentury/L10N.pm
@@ -0,0 +1,6 @@
+package MidCentury::L10N;
+use strict;
+use base 'MT::Plugin::L10N';
+
+1;
+
---
+++ plugins/MidCentury/lib/MidCentury/L10N/en_us.pm
@@ -0,0 +1,10 @@
+package MidCentury::L10N::en_us;
+
+use strict;
+
+use base 'MidCentury::L10N';
+use vars qw( %Lexicon );
+%Lexicon = ();
+
+1;
+
